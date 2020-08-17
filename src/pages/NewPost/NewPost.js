38bf4b6bf6111eb476020f3d66212443b679aff0
Furtado <furttado@gmail.com>
2020-08-17 18:12:36 -0300

    form state done

--- src/pages/NewPost/NewPost.js
+++ src/pages/NewPost/NewPost.js
@@ -6,24 +6,29 @@ import { RouteContext } from "../../hooks/routeContext";
 
 import { Button, TextField, Select } from "@material-ui/core";
 import { ArrowBackIos } from "@material-ui/icons";
+import { transparent } from "material-ui/styles/colors";
 
 const NewPost = () => {
   const classes = useStyles();
   const [routeValue, setRouteValue] = useContext(RouteContext);
   const [inputValue, setInputValue] = useState("");
-
-  function handleChange(event) {
-    setInputValue(event.target.value);
-  }
+  const initialFormData = { location: "", picture: "", description: "", role: "" };
+  const [formData, setFormData] = useState(initialFormData);
 
   function handleGoBack() {
     setRouteValue(history.goBack);
   }
 
+  function handleChange(event) {
+    const { id, value } = event.target;
+    setFormData({ ...formData, [id]: value });
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    handleGoBack();
+    console.log("formData", formData);
     console.log("Post done!");
+    handleGoBack();
   }
 
   const form = (
@@ -31,39 +36,42 @@ const NewPost = () => {
       <form className={classes.form}>
         <TextField
           required
-          value={""}
           variant="outlined"
-          id=""
           label="Location"
           type="text"
           className={classes.textField}
-          onChange={() => {}}
+          id="location"
+          value={formData.location}
+          onChange={handleChange}
         />
         <TextField
           required
-          value={""}
           variant="outlined"
-          id=""
           label="Picture url"
           type="text"
           className={classes.textField}
+          id="picture"
+          value={formData.picture}
+          onChange={handleChange}
         />
         <TextField
           required
-          value={""}
           variant="outlined"
-          id=""
           label="Description"
           type="text"
           multiline
           className={classes.textField}
+          id="description"
+          value={formData.description}
+          onChange={handleChange}
         />
         <Select
           native
-          value={inputValue}
-          onChange={handleChange}
           variant="outlined"
           className={classes.textField}
+          id="role"
+          value={formData.role}
+          onChange={handleChange}
         >
           <option value="" disabled>
             privacy setting

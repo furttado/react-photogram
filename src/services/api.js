d7a6625b9c20cfb43d6ac6b7366b73c5cafeec18
Furtado <furttado@gmail.com>
2020-09-09 11:12:44 -0300

    api baseURL update

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
-  //baseURL: "https://social-network-v1.herokuapp.com",
+  //baseURL: "http://localhost:3001",
+  baseURL: "https://social-network-v1.herokuapp.com",
 });
 
 api.interceptors.request.use(async (config) => {
